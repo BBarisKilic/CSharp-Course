5f8535adf3e4784b03bb80467ff5b5f380b59430
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-02-21 23:05:14 +0300

    Conditionals

---
+++ Conditionals/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Note: Ctrl+K, Ctrl+D => Reformat code
+            //Note: Ctrl+K, Ctrl+C => Comment
+            //Note: Ctrl+K, Ctrl+U => Uncomment
+
+            var number = 11;
+
+            //if - else
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+
+            else
+            {
+                Console.WriteLine("Number is not 10");
+            }
+
+            //single line if
+            Console.WriteLine(number == 10 ? "Number is 10" : "Number isn't 10");
+
+            //else if
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+            else if (number == 20)
+            {
+                Console.WriteLine("Number is 20");
+            }
+            else
+            {
+                Console.WriteLine("Number is neither 10 nor 20");
+            }
+
+            //switch
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is neither 10 nor 20");
+                    break;
+            }
+
+            //&& and ||
+            if (number >= 0 && number <= 100)
+            {
+                Console.WriteLine("Number is between 0-100");
+            }
+            else if (number > 100 && number <= 200)
+            {
+                Console.WriteLine("Number is between 101-200");
+            }
+            else if (number > 200 || number < 0)
+            {
+                Console.WriteLine("Number is less than 0 or greater than 200");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -18,14 +18,25 @@ namespace TypesAndVariables
             int number3 = 2147483647;
             long number4 = 9223372036854775807;
             double number5 = 21.4;
+            decimal number6 = 10.5m;
+            var number7 = 10;
+            number7 = 'A';
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
             Console.WriteLine("Number4 is {0}", number4);
             Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number6 is {0}", number6);
+            Console.WriteLine("Number7 is {0}", number7);
             Console.WriteLine(character);
             Console.WriteLine(condition);
+            Console.WriteLine(Days.Friday);
             Console.ReadKey();
         }
+    }
+
+    enum Days
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
     }
 }
